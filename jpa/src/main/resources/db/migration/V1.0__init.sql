98038033bcbcc580e485de180c23775bf8ceb17e
taehwankim <wih0202@gmail.com>
2022-02-19 01:32:28 +0900

    Revert "init"
    This reverts commit e9019e1dc18b6bb8d36bd25f62608bdb95d9a5c9.

--- jpa/src/main/resources/db/migration/V1.0__init.sql
+++ jpa/src/main/resources/db/migration/V1.0__init.sql
@@ -1,27 +1,27 @@
-CREATE TABLE DELIVERY_COMPANY
+CREATE TABLE delivery_company
 (
-    ID BIGINT AUTO_INCREMENT PRIMARY KEY
+    id bigint auto_increment PRIMARY KEY
 );
 
-CREATE TABLE DELIVERY_STRIKE_ZIPCODE
+CREATE TABLE delivery_strike_zipcode
 (
-    ID                  BIGINT AUTO_INCREMENT PRIMARY KEY,
-    DELIVERY_COMPANY_ID INT NOT NULL,
-    ZIPCODE             VARCHAR(100) DEFAULT '' NOT NULL,
-    ACTIVE              TINYINT(1) DEFAULT 1 NOT NULL,
-    CONSTRAINT ZIPCODE UNIQUE (ZIPCODE),
-    CONSTRAINT FK_DELIVERY_STRIKE_ZIPCODE_DELIVERY_COMPANY FOREIGN KEY (DELIVERY_COMPANY_ID) REFERENCES DELIVERY_COMPANY (ID)
+    id                  bigint auto_increment PRIMARY KEY,
+    delivery_company_id int NOT NULL,
+    zipcode             varchar(100) DEFAULT '' NOT NULL,
+    active              tinyint(1) DEFAULT 1 NOT NULL,
+    CONSTRAINT zipcode UNIQUE (zipcode),
+    CONSTRAINT fk_delivery_strike_zipcode_delivery_company foreign key (delivery_company_id) references delivery_company (id)
 );
 
-CREATE TABLE SAMPLE_ENTITY (
-    ID BIGINT NOT NULL AUTO_INCREMENT,
-    ADDRESS VARCHAR(255),
-    NAME VARCHAR(255),
-    PRIMARY KEY (ID)
+create table sample_entity (
+    id bigint not null auto_increment,
+    address varchar(255),
+    name varchar(255),
+    primary key (id)
 );
 
-INSERT INTO SAMPLE_ENTITY (ADDRESS, NAME) VALUES ( '경기도 고양시 1', '김태환' );
-INSERT INTO SAMPLE_ENTITY (ADDRESS, NAME) VALUES ( '경기도 고양시 2', '홍길동' );
-INSERT INTO SAMPLE_ENTITY (ADDRESS, NAME) VALUES ( '경기도 고양시 3', '차승원' );
-INSERT INTO SAMPLE_ENTITY (ADDRESS, NAME) VALUES ( '경기도 고양시 4', '태사자' );
-COMMIT;
+insert into sample_entity (address, name) values ( '경기도 고양시 1', '김태환' );
+insert into sample_entity (address, name) values ( '경기도 고양시 2', '홍길동' );
+insert into sample_entity (address, name) values ( '경기도 고양시 3', '차승원' );
+insert into sample_entity (address, name) values ( '경기도 고양시 4', '태사자' );
+commit;
