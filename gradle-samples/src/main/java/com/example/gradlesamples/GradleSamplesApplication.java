f57e675a7a7fcac676166e0675f3cb1277fb5f4e
taehwankim <wih0202@gmail.com>
2022-02-28 01:31:20 +0900

    gradle

---
+++ gradle-samples/src/main/java/com/example/gradlesamples/GradleSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradlesamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleSamplesApplication.class, args);
+    }
+
+}
---
+++ gradle-samples/src/test/java/com/example/gradlesamples/GradleSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlesamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
