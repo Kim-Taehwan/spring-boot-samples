668268869fd1529bd2e63912559d1e34591173c0
dave <thkim02@trenbe.com>
2022-08-19 16:57:13 +0900

    init

---
+++ mvn-jpa-boot/src/main/java/com/example/mvnjpaboot/MvnJpaBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvnjpaboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnJpaBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvnJpaBootApplication.class, args);
+    }
+
+}
---
+++ mvn-jpa-boot/src/test/java/com/example/mvnjpaboot/MvnJpaBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvnjpaboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnJpaBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
