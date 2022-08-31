0665d4f307ec20b81e5b7827f75d953e75ab204c
taehwankim <wih0202@gmail.com>
2022-09-01 00:04:53 +0900

    dbconfig init

---
+++ sample-dbconfig/src/main/java/com/example/sampledbconfig/SampleDbconfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampledbconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDbconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleDbconfigApplication.class, args);
+    }
+
+}
---
+++ sample-dbconfig/src/test/java/com/example/sampledbconfig/SampleDbconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampledbconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDbconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
